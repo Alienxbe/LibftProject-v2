96c6c55fded94f30cd93f035a1a0322f3cb7e6c7
Alienxbe <martin.ykman@gmail.com>
2022-03-05 04:07:39 +0100

    Corrected ctype tester and ft_isdigit (Now tester only test for Unsigned char characters)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 21:14:32 by mykman            #+#    #+#             */
-/*   Updated: 2022/03/03 21:14:32 by mykman           ###   ########.fr       */
+/*   Updated: 2022/03/05 04:04:50 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,15 @@
 
 void	ft_ctype_tester(void)
 {
-	int	(*ft_ctype[])(int) = {&ft_isalnum, &ft_isalpha, &ft_isascii,
+	int		(*ft_ctype[])(int) = {&ft_isalnum, &ft_isalpha, &ft_isascii,
 		&ft_isdigit, ft_isprint};
-	int	(*ctype[])(int) = {&isalnum, &isalpha, &isascii, &isdigit, &isprint};
+	int		(*ctype[])(int) = {&isalnum, &isalpha, &isascii, &isdigit, &isprint};
+	char	*f_name[] = {"ft_isalnum", "ft_isalpha", "ft_isascii", "ft_isdigit", "ft_isprint"};
 
 	for (int i = 0; i < 5; i++)
-		for (int c = CHAR_MIN; c <= CHAR_MAX; c++)
-			if (ft_ctype[i](c) != ctype[i](c))
-				printf("Error on function %d with character : `%d`\n", i, c);
+		for (int c = 0; c <= UCHAR_MAX; c++)
+			if (!ft_ctype[i](c) != !ctype[i](c))
+				printf("Error on function %s with character : `%d` (Original : `%d`, yours : `%d`)\n", f_name[i], c, !!ctype[i](c), !!ft_ctype[i](c));
 }
 
 int main(void)
