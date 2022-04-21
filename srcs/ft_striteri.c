8d192d4fbf27f68f95d4fd28db964549978435f4
Alienxbe <martin.ykman@gmail.com>
2022-04-21 17:15:16 +0200

    Corrected protecion : - ft_striteri

--- srcs/ft_striteri.c
+++ srcs/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: maykman <maykman@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 15:26:57 by mykman            #+#    #+#             */
-/*   Updated: 2022/04/21 17:07:56 by maykman          ###   ########.fr       */
+/*   Updated: 2022/04/21 17:15:02 by maykman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	size_t	i;
 
-	if (!s && !f)
+	if (!s || !f)
 		return ;
 	i = -1;
 	while (s[++i])
