1210696fb542a2335e2e6017ce872e9c1dbb8d44
Alienxbe <martin.ykman@gmail.com>
2022-04-21 17:08:47 +0200

    Made some changes : - Striteri should not return a value - Removed calloc's protection for Moulinette

--- srcs/ft_calloc.c
+++ srcs/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: maykman <maykman@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 19:12:55 by mykman            #+#    #+#             */
-/*   Updated: 2022/04/14 20:37:13 by maykman          ###   ########.fr       */
+/*   Updated: 2022/04/21 17:02:48 by maykman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,9 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
-	size_t	tot;
 
-	tot = nmemb * size;
-	if (nmemb != 0 && (tot / nmemb != size || tot % nmemb != 0))
-		return (NULL);
-	ptr = malloc(tot);
+	ptr = malloc(nmemb * size);
 	if (ptr)
-		ft_bzero(ptr, tot);
+		ft_bzero(ptr, nmemb * size);
 	return (ptr);
 }
